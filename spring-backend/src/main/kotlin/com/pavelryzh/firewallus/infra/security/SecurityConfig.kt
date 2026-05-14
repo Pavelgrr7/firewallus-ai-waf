c04481ade51fa3401b7acf6456dd30f6f656b238
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-05-14 02:43:57 -0500

    fix(auth): opened 8080 port for admin panel

--- spring-backend/src/main/kotlin/com/pavelryzh/firewallus/infra/security/SecurityConfig.kt
+++ spring-backend/src/main/kotlin/com/pavelryzh/firewallus/infra/security/SecurityConfig.kt
@@ -44,7 +44,7 @@ class SecurityConfig(private val jwtFilter: JwtFilter) {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:5173", "http://localhost:3000")
+        configuration.allowedOrigins = listOf("http://localhost:5173", "http://localhost:3000", "http://localhost:3001")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
