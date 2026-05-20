7737d70d6eadc7b80363fdde467928f53b88aed6
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-05-20 13:24:52 -0500

    feat(utils): updated normalizePayload method

--- ktor-service/src/main/kotlin/core/TrafficUtils.kt
+++ ktor-service/src/main/kotlin/core/TrafficUtils.kt
@@ -5,12 +5,13 @@ import java.net.URLDecoder
 
 fun normalizePayload(input: String): String {
     var normalized = input
-
-    normalized = runCatching { URLDecoder.decode(normalized, "UTF-8") }.getOrDefault(normalized)
+    var prev: String
+    do {
+        prev = normalized
+        normalized = runCatching { URLDecoder.decode(normalized, "UTF-8") }.getOrDefault(normalized)
+    } while (normalized != prev)
 
     normalized = normalized.replace(Regex("\\s+"), " ")
-
     normalized = normalized.replace("/./", "/").replace("/../", "/")
-
     return normalized.lowercase()
 }
