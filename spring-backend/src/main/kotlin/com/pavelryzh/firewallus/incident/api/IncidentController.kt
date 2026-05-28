e168e8a2ecb4bfbaac9970938a78fc58bbf6291d
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-05-28 04:31:09 -0500

    fix(api): minor incident api fixes

--- spring-backend/src/main/kotlin/com/pavelryzh/firewallus/incident/api/IncidentController.kt
+++ spring-backend/src/main/kotlin/com/pavelryzh/firewallus/incident/api/IncidentController.kt
@@ -4,6 +4,7 @@ import com.pavelryzh.firewallus.incident.service.IncidentNotificationService
 import com.pavelryzh.firewallus.incident.service.IncidentService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -26,7 +27,7 @@ class IncidentController(private val incidentService: IncidentService,
 
     @GetMapping
     fun getAllIncidents(
-        @PageableDefault(size = 20, page = 0) pageable: Pageable
+        @PageableDefault(size = 20, page = 0, sort = ["timestamp"], direction = Sort.Direction.DESC) pageable: Pageable
     ): Page<IncidentResponseDto> {
 
         val incidentsPage = incidentService.getAllIncidents(pageable)
